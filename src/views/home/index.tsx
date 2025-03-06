b3978f6d90f9dfd21f91bf623dcd0b074741c140
Hassan Zaib <hassan.zaib223@gmail.com>
2025-03-06 20:39:56 +0500

    image slider

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -16,6 +16,7 @@ export const HomeView: FC = ({ setOpenCreateModel }) => {
                 {/* <div className="bg-primary/10 -z-1 start-0 absolute top-0 h-14 w-14 animate-[spin_10s_linear_infinite] rounded-2xl rounded-br-none rounded-tl-none"></div> */}
                 <div className="absolute bottom-0 rounded-full bg-primary/20 -z-1 end-0 h-14 w-14 animate-ping"></div>
 
+                {/* left side section */}
                 <div className="">
                   <span className="z-10 px-3 py-1 text-sm font-medium tracking-wider uppercase rounded-md text-primary bg-primary/20">
                     CREATE SOLANA TOKEN {pkg.version}
@@ -31,7 +32,7 @@ export const HomeView: FC = ({ setOpenCreateModel }) => {
                   <div className="new_add_css">
                     <a
                       onClick={() => setOpenCreateModel(true)}
-                      className="inline-flex items-center justify-center gap-2 px-1 mt-10 text-white transition-all duration-500 border rounded-full pe-4 hover:bg-primary-hover group border-white/10"
+                      className="inline-flex items-center justify-center gap-2 px-1 py-1 mt-10 text-white transition-all duration-300 border rounded-full pe-4 hover:bg-primary-hover group border-white/10"
                     >
                       <span className="flex items-center justify-center rounded-full bg-primary/20 text-primary me-2 h-11 w-11 group-hover:bg-white/10 group-hover:text-white">
                         <i data-lucide="image" className="">
@@ -44,6 +45,79 @@ export const HomeView: FC = ({ setOpenCreateModel }) => {
                     <a className="mt-8">
                       <WalletMultiButton />
                     </a>
+                  </div>
+                </div>
+
+                {/* right side section */}
+                <div className="mx-auto h-[595px] overflow-hidden ">
+                  <div className="grid grid-cols-2 gap-6 marquee ">
+                    {/* left images slider */}
+                    <div className="relative flex flex-col gap-6 m-auto overflow-hidden">
+                      {/* first slider */}
+                      <div className="flex flex-col items-center justify-around flex-shrink-0 min-h-full gap-6 marquee-hero">
+                        {["img-6", "img-7", "img-8", "img-9", "img-10"].map(
+                          (image, index) => (
+                            <img
+                              key={index}
+                              src={`assets/images/ai/${image}.jpg`}
+                              alt={image}
+                              className="object-cover h-40 aspect-1 w-60 rounded-xl"
+                            />
+                          )
+                        )}
+                      </div>
+
+                      {/* second slider */}
+                      <div
+                        aria-hidden="true"
+                        className="flex flex-col items-center justify-around flex-shrink-0 min-h-full gap-6 marquee-hero"
+                      >
+                        {["img-11", "img-12", "img-13", "img-14", "img-21"].map(
+                          (image, index) => (
+                            <img
+                              key={index}
+                              src={`assets/images/ai/${image}.jpg`}
+                              alt={image}
+                              className="object-cover h-40 aspect-1 w-60 rounded-xl"
+                            />
+                          )
+                        )}
+                      </div>
+                    </div>
+
+                    {/* right images slider */}
+                    <div className="flex flex-col gap-6 m-auto overflow-hidden marquee-reverse ">
+                      {/* first slider */}
+                      <div className="flex flex-col items-center justify-around flex-shrink-0 min-h-full gap-6 marquee-hero">
+                        {["img-6", "img-10", "img-11", "img-12", "img-13"].map(
+                          (image, index) => (
+                            <img
+                              key={index}
+                              src={`assets/images/ai/${image}.jpg`}
+                              alt={image}
+                              className="object-cover h-40 aspect-1 w-60 rounded-xl"
+                            />
+                          )
+                        )}
+                      </div>
+
+                      {/* second slider */}
+                      <div
+                        aria-hidden="true"
+                        className="flex flex-col items-center justify-around flex-shrink-0 min-h-full gap-6 marquee-hero"
+                      >
+                        {["img-9", "img-14", "img-21", "img-22", "img-10"].map(
+                          (image, index) => (
+                            <img
+                              key={index}
+                              src={`assets/images/ai/${image}.jpg`}
+                              alt={image}
+                              className="object-cover h-40 aspect-1 w-60 rounded-xl"
+                            />
+                          )
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
